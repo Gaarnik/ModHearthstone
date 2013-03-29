4bdb4824e9aadd609ea3fcf397d449481ae88af5
Gaarnik <TheGaarnik@gmail.com>
2013-03-29 10:12:32 -0700

    Setting up project

---
+++ hearthstone_common/gaarnik/hearthstone/common/ModHearthstone.java
@@ -0,0 +1,49 @@
+package gaarnik.hearthstone.common;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.ServerStarting;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "ModHearthstone")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class ModHearthstone {
+	// *******************************************************************
+	@Instance("ModHearthstone")
+	public static ModHearthstone instance = new ModHearthstone();
+
+	// *******************************************************************
+
+	// *******************************************************************
+
+	// *******************************************************************
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Init
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
+	@ServerStarting
+	public void serverStarting(FMLServerStartingEvent event) {
+		
+	}
+
+	// *******************************************************************
+
+}
