32a103a83f595f4da52a54f96a5d006cddc95be3
Gaarnik <TheGaarnik@gmail.com>
2013-03-30 07:02:15 -0700

    Implementer le "link" et la téléportation

--- hearthstone_common/gaarnik/hearthstone/common/item/HearthstoneItem.java
+++ hearthstone_common/gaarnik/hearthstone/common/item/HearthstoneItem.java
@@ -1,8 +1,12 @@
 package gaarnik.hearthstone.common.item;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
@@ -21,6 +25,74 @@ public class HearthstoneItem extends Item {
 	}
 
 	// *******************************************************************
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int par7, float par8, float par9, float par10) {
+		if(world.isRemote)
+			return false;
+		
+		int blockId = world.getBlockId(x, y, z);
+		
+		if(blockId == Block.bed.blockID) {
+			if(stack.hasTagCompound() == false) {
+				NBTTagCompound nbt = new NBTTagCompound();
+				stack.setTagCompound(nbt);
+			}
+			
+			NBTTagCompound nbt = stack.getTagCompound();
+			nbt.setInteger("bedX", x);
+			nbt.setInteger("bedY", y);
+			nbt.setInteger("bedZ", z);
+
+			Vec3 position = player.getPosition(1.0f);
+			
+			nbt.setDouble("playerX", position.xCoord);
+			nbt.setDouble("playerY", position.yCoord);
+			nbt.setDouble("playerZ", position.zCoord);
+			
+			nbt.setBoolean("initialized", true);
+			
+			player.addChatMessage("Hearthstone linked !");
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		if(world.isRemote)
+			return stack;
+		
+		if(stack.hasTagCompound() == false)
+			return stack;
+		
+		NBTTagCompound nbt = stack.getTagCompound();
+		
+		if(nbt.getBoolean("initialized")) {
+			nbt.setBoolean("initialized", false);
+			return stack;
+		}
+		
+		int x = nbt.getInteger("bedX");
+		int y = nbt.getInteger("bedY");
+		int z = nbt.getInteger("bedZ");
+		
+		int blockId = world.getBlockId(x, y, z);
+		
+		if(blockId != Block.bed.blockID) {
+			player.addChatMessage("Your bed is missing !");
+			return stack;
+		}
+		
+		player.addChatMessage("Teleporting to home ...");
+		
+		double xCoord = nbt.getDouble("playerX");
+		double yCoord = nbt.getDouble("playerY");
+		double zCoord = nbt.getDouble("playerZ");
+		
+		player.setPositionAndUpdate(xCoord, yCoord, zCoord);
+		
+		return stack;
+	}
 
 	// *******************************************************************
 
